d916d862f087095692bbde92cbd2eb328b27c85b
Den_k0 <denys.kotovskiy@gmail.com>
2024-11-27 10:58:00 +0200

    task.status renamed to task.is_completed

--- todoList/models.py
+++ todoList/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     content = models.TextField(max_length=512)  # -content - describes what you should do.
     date = models.DateTimeField(auto_now_add=True)  # -datetime, when a task was created
     deadline = models.DateTimeField(null=True, blank=True)  # -optional deadline datetime if a task should be done until some datetime
-    status = models.BooleanField()  # -the boolean field that marks if the task is done or not
+    is_completed = models.BooleanField()  # -the boolean field that marks if the task is done or not
     tags = models.ManyToManyField("Tag", related_name="tasks")  # -tags that are relevant for this task
 
 
