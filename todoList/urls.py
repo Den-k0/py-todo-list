df4fdde3c2d7e69acab925766c4a8b9aae0c73d6
Den_k0 <denys.kotovskiy@gmail.com>
2024-11-27 15:48:22 +0200

    TodoList v1

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,10 +1,27 @@
 from django.urls import path
 
-from todoList.views import index
-
+from todoList.views import (
+    index,
+    TaskCreateView,
+    TaskDeleteView,
+    TaskUpdateView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    toggle_task,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("toggle_task/<int:pk>/", toggle_task, name="toggle_task"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todolist"
--- todoList/views.py
+++ todoList/views.py
@@ -1,5 +1,73 @@
-from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+
+from todoList.models import Tag, Task
 
 
 def index(request):
-    return render(request, "todoList/index.html")
+    tasks = Task.objects.all()
+    paginator = Paginator(tasks, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        "tasks": page_obj,
+        "is_paginated": page_obj.has_other_pages(),
+        "page_obj": page_obj,
+        "paginator": paginator,
+    }
+    return render(request, "todoList/index.html", context=context)
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:index")
+    template_name = "todoList/task_form.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:index")
+    template_name = "todoList/task_form.html"
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todolist:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+    template_name = "todoList/tag_list.html"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:tag-list")
+    template_name = "todoList/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todolist:tag-list")
+    template_name = "todoList/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todolist:tag-list")
+
+
+def toggle_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = not task.is_completed
+    task.save()
+    return redirect('todolist:index')
