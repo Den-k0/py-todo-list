87f5348a4bdf6d5e563f2dec022460465a0d7ca3
Den_k0 <denys.kotovskiy@gmail.com>
2024-11-27 11:13:16 +0200

    Implement __str__

--- todoList/models.py
+++ todoList/models.py
@@ -8,6 +8,12 @@ class Task(models.Model):
     is_completed = models.BooleanField()  # -the boolean field that marks if the task is done or not
     tags = models.ManyToManyField("Tag", related_name="tasks")  # -tags that are relevant for this task
 
+    def __str__(self):
+        return self.content
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=32)  # Tag - a tag symbolizes the theme of the task and consists only of a name.
+
+    def __str__(self):
+        return self.name
